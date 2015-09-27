105e8e0940e5433efe21be5ff7bee1f66d5099cc
madolinn <madolinnsoup@gmail.com>
2015-09-26 21:30:20 -0400

    Initial Publish
    Get' em champ.

---
+++ scripts/core.js
@@ -0,0 +1,24 @@
+_cv = []; // Array of Canvasses
+
+//moduLoad("physics"); // Modules to load. Automatically appends .js
+
+_g = {}; // "Globals". Try not to use too much.
+
+
+moduLoad.ready = function() {
+	
+	console.log("All loaded up and ready to go!");
+	console.log(_cv.setupAll() + " canvasses found."); // Setup all canvasses. We only have one. It's called _cv[0].
+	
+	//setInterval(function() { main.step(); },1000/60);
+	
+}
+
+// Don't touch this shit. //
+
+_cv.setupAll = function() {
+	$("canvas").each(function(ind) {
+		_cv[ind] = $(this)[0].getContext('2d');
+	});
+	return $("canvas").length;
+}
