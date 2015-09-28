1e67d7851101aa98c199ace1133bc97b821a00c1
DiscountGamer <Darkmistx999@yahoo.com>
2015-09-28 07:24:15 -0700

    oh i fucked up... but i fixed it

--- scripts/core.js
+++ scripts/core.js
@@ -5,22 +5,22 @@ moduLoad("dummy"); // Modules to load. Automatically appends .js
 _g = {}; // "Globals". Try not to use too much.
 
 
-moduLoad.ready = function() {
-	
-	console.log("All loaded up and ready to go!");
-	console.log(_cv.setupAll() + " canvasses found."); // Setup all canvasses. We only have one. It's called _cv[0].
-	
-	dummy.helloworld();
-	
-	//setInterval(function() { main.step(); },1000/60);
-	
+moduLoad.ready = function () {
+
+    console.log("All loaded up and ready to go!");
+    console.log(_cv.setupAll() + " canvasses found."); // Setup all canvasses. We only have one. It's called _cv[0].
+
+    dummy.helloworld();
+
+    //setInterval(function() { main.step(); },1000/60);
+
 }
 
 // Don't touch this shit. //
 
-_cv.setupAll = function() {
-	$("canvas").each(function(ind) {
-		_cv[ind] = $(this)[0].getContext('2d');
-	});
-	return $("canvas").length;
+_cv.setupAll = function () {
+    $("canvas").each(function (ind) {
+        _cv[ind] = $(this)[0].getContext('2d');
+    });
+    return $("canvas").length;
 }
