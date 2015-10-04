238d65d37e73bf6ef1a7baed412c64f021c01bc6
madolinn <madolinnsoup@gmail.com>
2015-10-04 00:50:01 -0400

    Hacky Learning Time
    Make multiple dummies. Make fun stuff. Neglect proper usage of image
    loading. Yay.

--- scripts/core.js
+++ scripts/core.js
@@ -2,7 +2,7 @@ _cv = []; // Array of Canvasses
 
 moduLoad("dummy"); // Modules to load. Automatically appends .js
 
-_g = {}; // "Globals". Try not to use too much.
+_g = { dummies : [] }; // "Globals". Try not to use too much.
 
 
 moduLoad.ready = function () {
@@ -10,7 +10,14 @@ moduLoad.ready = function () {
     console.log("All loaded up and ready to go!");
     console.log(_cv.setupAll() + " canvasses found."); // Setup all canvasses. We only have one. It's called _cv[0].
 
-    dummy.helloworld();
+    _g.dummies.push(new dummy()); // Make two clones of Dummy. put them inside a constant so we can keep track of them.
+	_g.dummies.push(new dummy());
+	
+	_g.dummies[1].pos = [90, 90]; // Change the second clone
+	
+	for (var i = 0; i < _g.dummies.length; i++) { // Go through all the dummies and tell them to "helloworld"
+		_g.dummies[i].helloworld();
+	}
 
     //setInterval(function() { main.step(); },1000/60);
 
--- scripts/dummy.js
+++ scripts/dummy.js
@@ -1,15 +1,12 @@
 dummy = function () {
     this.fps = 30;
-}; //A constructor. Leave it blank if you don't care.
-
-//A function that "dummy" owns.
-//"dummy" is automatically constructed in the global scope,
-//but can also be constructed as individual objects as many times as you want.
+	this.pos = [20, 10];
+};
 
 //var dum = new dummy(); would make an instance totally seperate from the global one.
 
 
-dummy.helloworld = function () { //constructor . functionname = function ( arguments ) {
+dummy.prototype.helloworld = function () { //constructor . functionname = function ( arguments ) {
 
 
 
@@ -21,24 +18,16 @@ dummy.helloworld = function () { //constructor . functionname = function ( argum
     console.log(myImage);
     
 
-    myImage.onload = function () {
-        setInterval(function () {
-        _cv[0].drawImage(myImage, 20, 10, 50, 50)
+   // myImage.onload = function () {	// Got rid of all this because we need to not have 1000 objects each
+										// setting their own timers. Be creative and decide how to get all the objects to draw on one timer.
+       // setInterval(function () {
+		   
+		   //Only reason we can neglect the onload is because Chrome and Firefox have the images cached so they're always loaded.
+		   
+        _cv[0].drawImage(myImage, this.pos[0], this.pos[1], 50, 50)
         
 			
-        }, this.fps);
-    }
-
-
-
-    //To make an image you'll want to create the object.
-    //var img = new Image(); then give it a link to the actual file.
-
-    //https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/Image
-
-    //Using Canvasses, you can use the _cv[number] (0 or 1, because we have 2 canvasses) object to draw the image.
-
-    //http://www.w3schools.com/tags/canvas_drawimage.asp
-
+        //}, this.fps);
+   // }
 }
 
